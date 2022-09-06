aaeb9c7a1a1e4f5b3321003ed562f68a3b3410aa
Kimmy <kimmy7526@gmail.com>
2022-09-06 13:23:06 +0800

    串接登入API

--- 01.js
+++ 01.js
@@ -1,15 +1,46 @@
 //Panel
-const panels=document.querySelectorAll(".panel");
-panels.forEach((panel)=>{
-    panel.addEventListener("click",toggleOpen);
-    panel.addEventListener("transitionend",toggleActive);
-});
+const panels = document.querySelectorAll(".panel")
+panels.forEach((panel) => {
+    panel.addEventListener("click", toggleOpen)
+    panel.addEventListener("transitionend", toggleActive)
+})
 
-function toggleOpen(){
-    this.classList.toggle("open");
+function toggleOpen() {
+    this.classList.toggle("open")
 }
-function toggleActive(e){
+function toggleActive(e) {
     console.log(e.propertyName);
-    if(e.propertyName!=="flex-grow") return;
-    this.classList.toggle("open-active");
+    if (e.propertyName !== "flex-grow") return
+    this.classList.toggle("open-active")
+}
+
+//login (以六角AJAX作為練習)
+//lovef2e@hexschool.com
+//12345678
+let login = document.querySelector('#logIn')
+login.addEventListener('click', loginCheck)
+
+function loginCheck() {
+    let email = document.querySelector('#email').value
+    let password = document.querySelector('#password').value
+    let account = {}
+    account.email = email
+    account.password = password
+    // console.log(account)
+    
+    let xhr = new XMLHttpRequest()
+    xhr.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signIn')
+    xhr.setRequestHeader('Content-type', 'application/json')
+    let data = JSON.stringify(account)
+    xhr.send(data)
+    xhr.onload = function () {
+        let callbackData = JSON.parse(xhr.responseText)
+        let msg = callbackData.message;
+        if (msg == '登入成功') {
+            alert('登入成功')
+        } else {
+            alert('此帳號不存在或帳號密碼錯誤')
+        }
+
+    }
 }
